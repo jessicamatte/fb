1e280399f7391a6698de9a1232a87230ed145c9a
Jessica Matte <jessicasmatte@gmail.com>
2022-03-28 14:48:29 -0300

    first steps

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB3ApFaJRfuthzgbutKSDme9u-qrb2RHfU",
+    authDomain: "facebook-cloned.firebaseapp.com",
+    projectId: "facebook-cloned",
+    storageBucket: "facebook-cloned.appspot.com",
+    messagingSenderId: "234669009816",
+    appId: "1:234669009816:web:cec17ce4ee0d196dc059ed"
+  };
